63cbe782b4655abc265715abe13e6d691286f18c
Sandra Couto <sandralmcouto@gmail.com>
2021-10-06 09:30:37 -0300

    add map component

---
+++ app/components/map.js
@@ -0,0 +1,7 @@
+import ENV from 'super-rentals/config/environment';
+
+export default class MapComponent extends Component {
+  get token() {
+    return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
+  }
+}
